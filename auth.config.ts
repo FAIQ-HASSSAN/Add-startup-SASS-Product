dba1b4d609dc8045b512ce7d328e108829caa2b4
Faseeh Hussain <faseehh227@gmail.com>
2025-07-25 14:42:18 +0500

    add secret

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,7 @@ export const authConfig : NextAuthConfig = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
         })
     ],
+   secret:process.env.NEXTAUTH_SECRET,
    pages : {signIn: '/signin'},
    callbacks:{}
 }
