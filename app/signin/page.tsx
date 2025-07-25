2477009c27445f80c61723c99a9edb68c898164a
Faseeh Hussain <faseehh227@gmail.com>
2025-07-25 16:11:56 +0500

    change

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,7 +6,7 @@ import { signIn } from 'next-auth/react';
 export default function SignInPage(){
     return(
         <div className='p-8'>
-            <h1>Sign In</h1>
+            <h1 className='heading'>Sign In</h1>
             <button className='bg-blue-500 text-white px-4 py-2 rounded' onClick={()=>signIn('google',{callbackUrl: '/dashboard'})}> 
                 Continue with Google
             </button>
